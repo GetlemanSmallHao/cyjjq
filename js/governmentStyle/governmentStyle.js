8b2d1ec01c7fb504bbd2300b1235787154190932
zxc1314 <a7389069@qq.com>
2018-05-21 11:34:15 +0800

    大事记轮播图

--- js/governmentStyle/governmentStyle.js
+++ js/governmentStyle/governmentStyle.js
@@ -1,9 +1,10 @@
 var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
-    autoplay:true,
-    effect: 'flip',
+    autoplay:true, //自动轮播
+    autoplayDisableOninteraction: false,  //用户点击之后不停止自动轮播,true为用户点击后停止自动轮播  
+    effect: 'flip', //切换效果
     grabCursor: true,
-    speed: 2000,
+    speed: 2000,  //滑块切换速度
     // 如果需要分页器
     pagination: {
       el: '.swiper-pagination',
