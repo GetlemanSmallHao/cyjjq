e0af14aeb6b17c4ebccce869fd43fc38c16f46e3
Mr.Yang <745585125@qq.com>
2018-05-22 07:51:16 +0800

    changed2

--- js/governmentStyle/governmentStyle.js
+++ js/governmentStyle/governmentStyle.js
@@ -1,7 +1,7 @@
 var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
     autoplay:true, //自动轮播
-    autoplayDisableOninteraction: false,  //用户点击之后不停止自动轮播,true为用户点击后停止自动轮播  
+    autoplayDisableOnInteraction: false,  //用户点击之后不停止自动轮播,true为用户点击后停止自动轮播
     effect: 'flip', //切换效果
     grabCursor: true,
     speed: 2000,  //滑块切换速度
